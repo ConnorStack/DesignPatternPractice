5c33aceccf87df9436adb6e97c6fbc2c424b985d
unknown <connorjstack.wrk@gmail.com>
2023-11-04 22:37:31 -0500

    abstract shape parent class

---
+++ ShapeFactory/shape-factory/src/main/java/me/designpatterns/Shapes/Shape.java
@@ -0,0 +1,12 @@
+package me.designpatterns.Shapes;
+
+public abstract class Shape{
+    private String name;
+    private double area;
+
+    public abstract double calculateArea();
+
+    public void displayInfo();
+
+
+}
