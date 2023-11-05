241b3a6daa81421f233188ae58bad5bfa38605e6
unknown <connorjstack.wrk@gmail.com>
2023-11-04 22:47:44 -0500

    adding rectangle

--- ShapeFactory/shape-factory/src/main/java/me/designpatterns/App.java
+++ ShapeFactory/shape-factory/src/main/java/me/designpatterns/App.java
@@ -1,5 +1,9 @@
 package me.designpatterns;
 
+import java.text.DecimalFormat;
+
+import me.designpatterns.Shapes.Circle;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Circle circle = new Circle(5);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        double circleArea = circle.calculateArea();
+        String firmattedArea = decimalFormat.format(circleArea);
+        System.out.println("Area is: " + firmattedArea);
     }
 }
