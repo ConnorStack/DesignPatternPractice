40c6917709d71c5964fedd7beced1e117d7ad4f7
unknown <connorjstack.wrk@gmail.com>
2023-11-16 22:37:49 -0600

    implementing a Dog superclass

--- ShapeFactory/shape-factory/src/main/java/me/designpatterns/Introduction/Dog.java
+++ ShapeFactory/shape-factory/src/main/java/me/designpatterns/Introduction/Dog.java
@@ -1,5 +1,14 @@
 package me.designpatterns.Introduction;
 
 abstract public class Dog {
-    
+    BarkBehavior barkBehavior;
+    PlayBehavior playBehavior;
+
+    public void performBark(){
+        barkBehavior.bark();
+    }
+
+    public void performPlay(){
+        playBehavior.play();
+    }
 }
