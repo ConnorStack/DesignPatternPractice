ae9d1b220db74bc6dc51419168e94b0873360dd4
unknown <connorjstack.wrk@gmail.com>
2023-11-17 22:24:57 -0600

    implementing SoldOutState

--- HeadStartDesignPatterns/src/main/java/practice/designpatterns/StatePattern/SoldOutState.java
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/StatePattern/SoldOutState.java
@@ -1,21 +1,25 @@
 package practice.designpatterns.StatePattern;
 
 public class SoldOutState implements State {
-    
+    GumballMachine gumballMachine;
+
+    public SoldOutState(GumballMachine gumballMachine){
+        this.gumballMachine = gumballMachine;
+    }
     
     public void insertQuarter() {
-
+        System.out.println("Sold out");
     }
 
     public void ejectQuarter() {
-
+        System.out.println("Sold out");
     }
 
     public void turnCrank() {
-
+        System.out.println("Sold out");
     }
 
     public void dispense() {
-
+        System.out.println("Sold out");
     }
 }
