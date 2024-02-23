25ae52a72f744d273762dec92725ba34046d7680
ConnorStack <connorjstack.wrk@gmail.com>
2024-02-23 13:01:39 -0600

    Create Subject interface method signatures

--- HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/Subject.java
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/Subject.java
@@ -1,5 +1,9 @@
 package practice.designpatterns.ObserverPattern;
 
 public interface Subject {
+    public void regesterObserver(Observer observer);
 
+    public void removeObserver(Observer observer);
+
+    public void notifyObservers();
 }
