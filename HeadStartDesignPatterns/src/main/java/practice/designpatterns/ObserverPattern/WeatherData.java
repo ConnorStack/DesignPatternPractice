84f78190992067970a2f6b49aa2f1e7159fd94fc
ConnorStack <connorjstack.wrk@gmail.com>
2024-02-23 13:28:52 -0600

    Implement WeatherData constructor to make ArrayList of Observers

--- HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/WeatherData.java
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/WeatherData.java
@@ -1,6 +1,18 @@
 package practice.designpatterns.ObserverPattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeatherData implements Subject {
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<Observer>();
+    }
+
     @Override
     public void regesterObserver(Observer observer) {
 
