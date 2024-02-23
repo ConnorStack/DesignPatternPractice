1b8b277625ae701f2fcdabf5f681c2406926a601
ConnorStack <connorjstack.wrk@gmail.com>
2024-02-23 13:02:32 -0600

    Create overrides in WeatherData for Subject

--- HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/WeatherData.java
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/WeatherData.java
@@ -1,5 +1,19 @@
 package practice.designpatterns.ObserverPattern;
 
 public class WeatherData implements Subject {
+    @Override
+    public void regesterObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
 
 }
