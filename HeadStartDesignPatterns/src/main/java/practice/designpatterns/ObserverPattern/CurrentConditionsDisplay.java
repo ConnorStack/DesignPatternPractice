3d60db7e527f32a1466eebd46ff35437331c20be
ConnorStack <connorjstack.wrk@gmail.com>
2024-02-23 13:20:06 -0600

    Create Observers update signature, override in CurrentConditionsDisplay

--- HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/CurrentConditionsDisplay.java
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/CurrentConditionsDisplay.java
@@ -1,5 +1,8 @@
 package practice.designpatterns.ObserverPattern;
 
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
-
+    @Override
+    public void update(float temp, float humidity, float pressure){
+        
+    }
 }
--- HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/Observer.java
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/ObserverPattern/Observer.java
@@ -1,5 +1,5 @@
 package practice.designpatterns.ObserverPattern;
 
 public interface Observer {
-
+    public void update(float temp, float humidity, float pressure);
 }
