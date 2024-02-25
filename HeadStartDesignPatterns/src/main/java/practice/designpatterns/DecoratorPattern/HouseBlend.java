4901080680d182afd6ffb168493b1a3fcc5e0091
ConnorStack <connorjstack.wrk@gmail.com>
2024-02-25 08:03:44 -0600

    Implement HouseBlend

---
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/DecoratorPattern/HouseBlend.java
@@ -0,0 +1,11 @@
+package practice.designpatterns.DecoratorPattern;
+
+public class HouseBlend extends Beverage {
+    public HouseBlend() {
+        description = "House Blend";
+    }
+
+    public double cost() {
+        return .89;
+    }
+}
