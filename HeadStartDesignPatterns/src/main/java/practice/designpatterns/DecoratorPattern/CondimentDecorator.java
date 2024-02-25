fa591b502784e996c6ef9d362594b44778c96645
ConnorStack <connorjstack.wrk@gmail.com>
2024-02-25 08:01:54 -0600

    Implement Espresso class

---
+++ HeadStartDesignPatterns/src/main/java/practice/designpatterns/DecoratorPattern/CondimentDecorator.java
@@ -0,0 +1,7 @@
+package practice.designpatterns.DecoratorPattern;
+
+public abstract class CondimentDecorator extends Beverage {
+    Beverage beverage;
+
+    public abstract String getDescription();
+}
